09a4ee8d98243ca75441234a2a3b2666b30618d0
MuhammadZargham2022 <98009558+MuhammadZargham2022@users.noreply.github.com>
2024-09-06 14:33:19 +0500

    Add files via upload

---
+++ Sidebar.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import './Sidebar.scss';
+import sidebarElement from './SideData';
+import { NavLink } from 'react-router-dom';
+import userImage from './assets/user.jpg';
+import {
+  PiCaretLeftBold,
+  PiCaretDownBold,
+
+} from "react-icons/pi";
+
+const Sidebar = () => {
+  const [isclose, setIsclose] = useState(false);
+  const [isMenuOpen, setisMenuOpen] = useState(false);
+
+  const ToggleSidebar = () => setIsclose(!isclose);
+  const ToggleSubMenu = () => setisMenuOpen(!isMenuOpen);
+
+
+  return (
+    <>
+      <div className="container" >
+        <div className={isclose ? "sidebar active" : "sidebar"}>
+
+          <div className="menu-btn" onClick={ToggleSidebar}>
+            <PiCaretLeftBold />
+          </div>
+
+          <div className="head">
+            <div className="user-img">
+              <img src={userImage} alt="user" />
+            </div>
+            <div className="user-details">
+              <p className="title">Web Developer</p>
+              <p className="name">Sheeda Meeda</p>
+            </div>
+          </div>
+          <div className="nav">
+            <div className="menu">
+
+              {sidebarElement.map((el) => {
+                return (
+                  <>
+                    <p className="title">{el.menuHeading}</p>
+                    <ul>
+                      <li>
+                          <NavLink to={el.path} onClick={el.title == 'Audience' ? ToggleSubMenu : ""}>
+
+                            <i className="icon"> {<el.icon />}</i>
+                            <span className="text">{el.title}</span>
+                            <span>
+                              <i className="arrow"> {el.title == 'Audience' ? <PiCaretDownBold /> : ""}</i>
+                            </span>
+
+
+                          </NavLink>
+                        {el.submenu && (
+                          <ul className={isMenuOpen ? "sub-menu-open" : "sub-menu"}>
+                            {el.submenu.map((subEl) => {
+                              return (
+                                <li>
+                                  <NavLink to={subEl.subpath}>
+                                    <span className="text">{subEl.subtitle}</span>
+                                  </NavLink>
+                                </li>
+                              );
+                            })}
+                          </ul>
+                        )}
+                      </li>
+                    </ul>
+                  </>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+export default Sidebar
